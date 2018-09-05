5c01523a7e0114f73492cad427fc7511e647ba16
“Andrew <andrewwoods88@gmail.com>
2018-09-05 11:26:17 -0400

    installed gritter for growl notifications

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,9 +10,10 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
+//= require jquery3
 //= require rails-ujs
 //= require turbolinks
-//= require jquery3
+//= require gritter
 //= require popper
 //= require bootstrap-sprockets
 //= require_tree .
