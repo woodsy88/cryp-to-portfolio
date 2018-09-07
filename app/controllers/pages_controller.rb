bd8ff5e2fa4a8aab5807ccadc9db2474e794975d
“Andrew <andrewwoods88@gmail.com>
2018-09-07 11:14:14 -0400

    Searching for a specific crypto currency - loop over each returned object and compare it to  our search term to find a match

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -17,7 +17,25 @@ class PagesController < ApplicationController
   end
 
   def lookup
+    require 'net/http'
+    require 'json'
+    @url = 'https://api.coinmarketcap.com/v1/ticker/'
+    @uri = URI (@url)
+    @response = Net::HTTP.get(@uri)
+    @lookup_coin = JSON.parse(@response)
+   
     @symbol = params[:symbol]
+    
+    if @symbol
+      @symbol = @symbol.upcase
+    end
+    
+     if @symbol == ""
+      @symbol = "you need to enter a search querry"
+    end
+
+
+
   end
 
 end
