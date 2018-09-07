619d4e4cdaa18df6bdf5e8575a1ab98359a09d68
“Andrew <andrewwoods88@gmail.com>
2018-09-07 10:32:46 -0400

    created controller market api query and rendered in views

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,23 @@
 class PagesController < ApplicationController
   def home
+    require 'net/http'
+    require 'json'
+
+    @url = 'https://api.coinmarketcap.com/v1/ticker/'
+    @uri = URI (@url)
+    @response = Net::HTTP.get(@uri)
+    @coins = JSON.parse(@response)
+    @my_coins = ["BTC", "XRP", "ADA", "XLM", "STEEM"]
+
   end
+
+  def about
+
+  end
+
+  def lookup
+
+  end
+  
 end
+
