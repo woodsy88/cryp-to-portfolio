672612210d079f1a0ef2d2f75fd0f60ddb91b920
“Andrew <andrewwoods88@gmail.com>
2018-09-12 10:43:40 -0400

    Added the rescue-from method to pundit to redirect users who dont have access to an action

--- app/policies/crypto_policy.rb
+++ app/policies/crypto_policy.rb
@@ -1,37 +1,14 @@
 class CryptoPolicy < ApplicationPolicy
 
+  def edit?
+    user_who_can_access_post
+  end
+
   def update?
-    # return true if post_approved? && admin?
-    # return true if user_or_admin && !post_approved?
-    # record.user_id == user.id || admin_types.include?(user.type)
-    record.user_id == user.id
+    user_who_can_access_post
   end
 
-  def edit
+  def user_who_can_access_post
     record.user_id == user.id
   end
-
-  # def approve?
-  #   return true if admin?
-  # end
-
-  # def booked?
-  #   return true if user_or_admin
-  # end
-
-  # private 
-
-  # def user_or_admin
-  #   record.user_id == user.id || admin?
-  # end
-
-  # def admin?
-  #   admin_types.include?(user.type)
-  # end
-
-  # def post_approved?
-  #   record.approved?
-  # end
-
-
 end
